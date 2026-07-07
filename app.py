40bb5e1182d3f540c0095b6ef38b14b94f45e4fb
Shaikh Sadik <sadik889@gmail.com>
2026-07-08 00:40:29 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
